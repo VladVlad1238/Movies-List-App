babfdc251a6853ec97ba6b89602306b249688b59
Vladyslav Sikalo <vladislavmediol@gmail.com>
2023-06-07 20:25:47 +0200

    last update

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,31 @@
 
+
+const ZERO = 0;
+const TITLE_INPUT_LIMIT = 100;
+
 const tasksFormNode = document.querySelector('.js-tasks-form');
 const tasksInputNode = document.querySelector('.js-tasks-input');
 const tasksListNode = document.querySelector('.js-tasks-list');
-const addTaskButtonNode = document.querySelector('.js-add-tasks-btn');
+const addTasksButtonNode = document.querySelector('.js-add-tasks-btn');
 
 
 const addTasks = (event) => {
 
   event.preventDefault();
 
-  const getTaskText = () => {
-    const taskText = tasksInputNode.value;
-    return taskText;
+  const getTasksText = () => {
+    const tasksText = tasksInputNode.value;
+    return tasksText;
   };
   
-  const setTaskHTML = () => {
-    const setTask = getTaskText();
+  const setTasksHTML = () => {
+    const setTasks = getTasksText();
     const setHTML = ` <div class="box__container">
     <div class="first-col">
         <label class="box__lable">
             <input class="box__input" type="checkbox" data-action="done">
         </label>
-        <p class="task__name">${setTask}</p>
+        <p class="task__name">${setTasks}</p>
     </div>
     <div class="second-col">
         <button class="delete__button" data-action="delete">
@@ -32,7 +36,7 @@ const addTasks = (event) => {
   };
   
   const getTasksList = () => {
-    const getTasks = setTaskHTML();
+    const getTasks = setTasksHTML();
     tasksListNode.insertAdjacentHTML('beforeend', getTasks);
   };
 
@@ -52,35 +56,29 @@ const addTasks = (event) => {
   inputManipulation();
 };
 
-const disabledButton = () => {
-  if(tasksInputNode.value.length > 0) {
-    addTaskButtonNode.removeAttribute('disabled');
-  } else {
-    addTaskButtonNode.setAttribute('disabled', 'disabled');
-  }
-
-  if(tasksInputNode.value.length > 150) {
-    addTaskButtonNode.setAttribute('disabled', 'disabled');
-  }
-};
-
-
-
-
 const doneTask = (event) => {
   if(event.target.dataset.action === 'done') {
     const parentNode = event.target.closest('.box__container');
     parentNode.classList.toggle('box__container-done');
-  } ;
+  };
 };
 
-
-
 const deleteTask = (event) => {
-  
   if(event.target.dataset.action === 'delete') {
     const parentNode = event.target.closest('.box__container');
     parentNode.remove();
+  };
+};
+
+const disabledButton = () => {
+  if(tasksInputNode.value.length > ZERO) {
+    addTasksButtonNode.removeAttribute('disabled');
+  } else {
+    addTasksButtonNode.setAttribute('disabled', 'disabled');
+  };
+
+  if(tasksInputNode.value.length > TITLE_INPUT_LIMIT) {
+    addTasksButtonNode.setAttribute('disabled', 'disabled');
   };
 };
 
